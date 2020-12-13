8f3455cf5f9c48eeb2fcf109b60175ebd2976ddd
Bastien Aracil <findbastien@gmail.com>
2020-12-13 17:47:33 +0100

    clean up day13 part 2

--- perococco.aoc.problems/perococco.aoc.day13/src/main/java/perococco/aoc/day13/Day13Part2Solver.java
+++ perococco.aoc.problems/perococco.aoc.day13/src/main/java/perococco/aoc/day13/Day13Part2Solver.java
@@ -32,13 +32,23 @@ public class Day13Part2Solver extends Day13Solver {
                                  .map(Bus::id)
                                  .reduce(BigInteger.ONE, BigInteger::multiply);
 
-        // We can write a solution of the problem (that is not the minimal one) like this
+        // Let consider the number of the form
         //
-        // N = k1.(product/id_1) + k2.(product/id_2) + k3.(product/id_3) + ...
+        // N = k_1.(product/id_1) + k_2.(product/id_2) + k_3.(product/id_3) + ...
         //
-        // then N mod id_i = ki.(product/id_i)  this does not depend on the other ks
-        // and can be solved for each bus independently.
-        // the minimal solution is then obtain from N by take the modulo of N by 'product'
+        // where 'product' is the product of all the id_i and k_i are some values
+        // we can adjust
+        //
+        // then r_i the modulo of N by id_i is :
+        //
+        // r_i = N mod id_i = ki.(product/id_i)   (since 'product' is a multiple of all the other id)
+        //
+        // This does not depend on the other k.
+        // by finding the ki such as r_i = i, N will be a solution to the problem.
+        // then by taking the modulo of this solution by the 'product' we will get the
+        // minimal solution
+        //
+        // each equationm r_i can be solved independently from each other by using Ferma's little theorem
 
         return IntStream.range(0, buses.size())
                         .mapToObj(i -> solveForOneBus(buses.get(i), product, i))
@@ -52,10 +62,15 @@ public class Day13Part2Solver extends Day13Solver {
             return BigInteger.ZERO;
         }
         final var id = bus.id();
-        final var remaining = index == 0 ? 0 : id.intValue() - index;
+        final var remaining = BigInteger.valueOf(Tools.mod(id.intValue() - index,id.intValue()));
+
+        if (remaining.equals(BigInteger.ZERO)) {
+            return BigInteger.ZERO;
+        }
+
         final var productOfOthers = productOfAllIds.divide(id);
         //Fermat's little theorem says that : productOfOthers^(id-1) = 1 [id] (we assume all ids are prime)
-        return productOfOthers.pow(id.intValue() - 1).multiply(BigInteger.valueOf(remaining));
+        return productOfOthers.pow(id.intValue() - 1).multiply(remaining);
     }
 
 }
